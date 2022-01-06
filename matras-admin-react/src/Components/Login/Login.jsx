7863c6741826636694f7f1c530d9cb560f582dfe
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-01-06 17:52:36 +0500

    add login

---
+++ matras-admin-react/src/Components/Login/Login.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Account from "../../Assets/Images/Profile (1).png";
+import Lock from "../../Assets/Images/Lock.png";
+import "./Login.css";
+
+const Login = () =>{
+// const [btn,setBtn] = useState("button");
+let className ="form-login";
+let classNames ="form-password";
+const [error, setError] = useState("");
+
+// const handleBtn=(evt)=>{
+// evt.preventDefault();
+
+// const {login, parol} = evt.target.elements;
+
+
+
+// if (!login.value.trim()) {
+   
+//     className += "  error-input";
+
+//     console.log(className);
+// }
+
+
+// if (!parol.value.trim()) {
+   
+//     classNames += "  error-input"
+//     console.log(classNames);
+// }
+// } 
+
+return (
+<div className="login">
+    <div className="container">
+        <div className="login-box">
+            <h2 className="login-title">
+
+                Kirish
+            </h2>
+            <form className="login-form">
+                <label className="form-label">
+                    <img className="form-account" src={Account} alt="Account" />
+                    <input type="password" className={className}  placeholder="Login" name="login"/>
+
+                </label>
+
+                <label className="form-label">
+                    <img className="form-lock" src={Lock} alt="Lock" />
+                    <input type="password" className={classNames}  placeholder="Parol" name="parol"/>
+
+                </label> 
+
+                <button type="submit" className={`form-btn ${error ? error : ""} `} onClick={() => {
+                    setError("btn-submit") 
+                }} >
+                   Kirish
+                </button>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+)
+}
+
+export default Login;
