76c91d937d39acbd73805a6f3296eca1e6e2c78f
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-01-27 15:40:35 +0500

    add succesfully buyurtmalar,customers, and toifalar qo'shish

--- matras-admin-react/src/Components/Table/Table.jsx
+++ matras-admin-react/src/Components/Table/Table.jsx
@@ -1,19 +1,55 @@
-import  { useEffect } from "react";
+import  { useEffect, useState } from "react"; 
 import Toggle from "../Toggle/Toggle";
 import "../Toggle/Toggle.css"
+import constants from "../../configs/constants";
+
 
 // import TrComponent from "../TrComponent/TrComponent";
-import "./Table.css";
-import { useState } from "react";
+import "./Table.css"; 
 
 
 const Table = ({items}) =>{
-    // console.log(items,"aloooo")
-const [data,setData] = useState([]);
+    const [data, setData] = useState([]); 
 
-    useEffect(() =>{
-        fetch("https://jsonplaceholder.typicode.com/users").then((res) =>res.json()).then((data) => setData(data));
-    },[])
+    const getOrders = async() => {
+        let token = window.localStorage.getItem("token")
+
+		let response = await fetch(constants.API_URL+"/v1/orders",{
+			method: "GET", 
+			headers: {
+				"Content-Type": "application/json",
+                "Authorization": token
+			}
+		})
+ 
+		response = await response.json(); 
+		if(response?.data.order){
+            console.log(response?.data.order);
+            setData(response?.data.order)
+        }
+    } 
+
+    const deleteCustomerService = async(id) => {
+        let token = window.localStorage.getItem("token") 
+		let response = await fetch(constants.API_URL+`/v1/customers/delete/${id}`,{
+			method: "DELETE", 
+			headers: {
+				"Content-Type": "application/json",
+                "Authorization": token
+			}
+		})  
+
+        response = await response.json();
+        console.log(response);
+        if(response.ok == true) {
+            
+            getOrders()
+        }
+    } 
+
+    useEffect(async () => {
+       await getOrders()  
+    }, []) 
 
 return (
 <section className="tables">
@@ -34,27 +70,22 @@ return (
     <div className="tbl-content">
         <table>
             <tbody>
-                {data.length > 0 && data.map((row,i) =>(
+                {data.length > 0 && data.map((order,i) =>(
+                <tr className="tr" key={i}>
+                    <td className="td-left">{order.order_id}</td>
+                    <td>{order.user_name}</td>
+                    <td>{order.user_phone}</td>
+                    <td>{order.order_name}</td>
+                    <td>{order.order_amount}</td>
+                    <td className="td-right">
+                        <div className="toggle"> 
+                            <input type="checkbox" id={order.order_contacted} />
+                            <label className="toggle-label" htmlFor={order.order_contacted}>Toggle</label> 
+                        </div>
+                    </td>
+                </tr>
 
-
-                
-        <tr className="tr" key={i} >
-        <td className="td-left">{row.id}</td>
-        <td>{row.name}</td>
-        <td>{row.phone}</td>
-        <td>{row.username}</td>
-        <td>{row.id+2}</td>
-        <td className="td-right" >
-        <div className="toggle">
-
-<input type="checkbox"  id={row.id} />
-    <label className="toggle-label" for={row.id}>Toggle</label>
-
-</div>
-        </td>
-    </tr>
-         
-         ))}
+                ))}
             </tbody>
         </table>
 
--- matras-admin-react/src/Components/ToifalarTable/ToifalarTable.jsx
+++ matras-admin-react/src/Components/ToifalarTable/ToifalarTable.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
 import Edit from "../../Assets/Images/edit.png";
 import Close from "../../Assets/Images/Close.png";
 import "./ToifalarTable.css"
-import { useState } from "react";
 import Delete from "../../Assets/Images/delete.png";
-
 import Modal from "../Modal/Modal";
+import { useEffect, useState } from "react";
+import constants from "../../configs/constants";
 
-const ToifalarTable = () =>{
-// console.log(items,"aloooo")
-const [data,setData] = useState([]);
+
+
+const  ToifalarTable = () =>{
 const [deleteModal,setDeleteModal] = useState(false);
 const [editModal,setEditModal] = useState(false);
 const [addModal,setAddModal] = useState(false);
+const [cName, setCName] = useState("")
 
 
 function openDeleteModal(){
@@ -23,12 +23,71 @@ setEditModal(!editModal)
 }
 
 function openAddModal(){
-    setAddModal(!addModal)
+setAddModal(!addModal)
+}
+
+const [data, setData] = useState([]);
+
+const getCategories = async(id) => {
+let token = window.localStorage.getItem("token")
+
+let response = await fetch(constants.API_URL+"/v1/categories",{
+    method: "GET",
+    headers: {
+    "Content-Type": "application/json",
+    "Authorization": token
+}
+})
+
+response = await response.json();
+    if(response?.data.categories){ 
+    setData(response?.data.categories)
     }
+}
+
+const createCategory = async(name) => {
+    let token = window.localStorage.getItem("token")
     
-useEffect(() =>{
-fetch("https://jsonplaceholder.typicode.com/users").then((res) => res.json()).then((data) => setData(data))
-},[])
+    let response = await fetch(constants.API_URL+"/v1/categories",{
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Authorization": token
+        },
+        body: JSON.stringify({
+            category_name: name
+        })
+    })
+    
+    response = await response.json();
+        if(response?.data.categories){ 
+        getCategories()
+    }
+}
+
+const delete_category = async(id) => {
+    let token = window.localStorage.getItem("token")
+    let response = await fetch(constants.API_URL+`/v1/categories/${id}`,{
+    method: "DELETE",
+    headers: {
+    "Content-Type": "application/json",
+    "Authorization": token
+}
+})
+
+    response = await response.json();
+        console.log(response);
+        if(response.ok == true) {
+        openDeleteModal()
+        getCategories()
+    }
+}
+
+useEffect(async () => {
+await getCategories()
+}, [])
+
+
 
 return (
 <div className="toifalar-table">
@@ -50,17 +109,62 @@ return (
             <table>
                 <tbody>
 
-                    {data.map((row,i) =>(
+                    {data.length > 0 &&  data.map((category, i) =>(
                     <tr className="tr" key={i}>
-                        <td className="td-left">{row.username}</td>
+                        <td className="td-left">{category.category_name}</td>
                         <td className="td-right">
-                            <button className="edit-btn" key={row.id} onClick={()=> openEditModal()} >
+                            <button className="edit-btn"  onClick={()=> openEditModal()} >
                                 <img src={Edit} alt="" />
                             </button>
-                            <button className="delete-btn" key={row.id} onClick={()=> openDeleteModal()} >
+                            <button key={i} className="delete-btn"  onClick={()=> openDeleteModal()} >
                                 <img src={Delete} alt="" /></button>
-
                         </td>
+
+                        <Modal show={deleteModal} w={400} mh={120}>
+                            <div className="delete-box">
+                                <h2 className="delete-title">Haqiqatdan ham o'chirmoqchimisiz ?
+                                </h2>
+                                <div className="delete-footer">
+                                    <button className="delete-no" onClick={()=>openDeleteModal()}>
+                                        Yo'q
+                                    </button>
+                                    <button className="delete-yes" onClick={() => delete_category(category.category_id)} type="submit">
+                                        Ha
+                                    </button>
+                                </div>
+                            </div>
+                        </Modal>
+                        <Modal show={editModal} w={300}>
+                            <div className="modal-blok">
+                                {/* salom */}
+                                {/* <div className="modal-closes"> */}
+                                    <button className="close-btn" onClick={()=>setEditModal()}>
+                                        <img src={Close} className="close-icon" alt="" />
+                                    </button>
+                                    {/* </div> */}
+                                <h2 className="modal-title">Toifani tahrirlash</h2>
+                                <form className="modal-form">
+                                    <p className="modal-text">
+                                        Toifa nomi
+                                    </p>
+                                    <input type="text" className="modal-input" required
+                                        placeholder="masalan: Model A" />
+                                    <div className="modal-box">
+                                        <p className="modal-subtext">Holat</p>
+                                        <div className="toggle">
+
+                                            <input type="checkbox" id="holat" />
+                                            <label className="toggle-label" for="holat">Toggle</label>
+
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" className="modal-btn">
+                                        Qo'shish
+                                    </button>
+                                </form>
+                            </div>
+                        </Modal>
                     </tr>
                     ))}
 
@@ -69,84 +173,43 @@ return (
 
 
         </div>
-        <button className="btns" onClick={() => openAddModal() }>Qo'shish</button>
+        <button className="btns" onClick={()=> openAddModal() }>Qo'shish</button>
 
-        <Modal show={deleteModal} w={400} mh={120}>
-            <div className="delete-box">
-                <h2 className="delete-title">Haqiqatdan ham o'chirmoqchimisiz ?
-                </h2>
-                <div className="delete-footer">
-                    <button className="delete-no" onClick={()=>openDeleteModal()}>
-                        Yo'q
-                    </button>
-                    <button className="delete-yes" type="submit">
-                        Ha
-                    </button>
-                </div>
-            </div>
-        </Modal>
-        <Modal show={editModal} w={300}>
+
+
+        <Modal show={addModal} w={300}>
             <div className="modal-blok">
-                {/* salom */}
-                {/* <div className="modal-closes"> */}
-                    <button className="close-btn" onClick={()=>setEditModal()}>
-                        <img src={Close} className="close-icon" alt="" />
-                    </button>
-                    {/* </div> */}
-                <h2 className="modal-title">Toifani tahrirlash</h2>
+
+                <button className="close-btn" onClick={()=>setAddModal()}>
+                    <img src={Close} className="close-icon" alt="" />
+                </button>
+
+                <h2 className="modal-title">Qo'shish</h2>
                 <form className="modal-form">
                     <p className="modal-text">
-                        Toifa nomi
+                        Kategoriya nomi
                     </p>
-                    <input type="text" className="modal-input" required placeholder="masalan: Model A" />
+                    <input type="text" className="modal-input"   onChange={(e) => {
+                        setCName(e.target.value)
+                    }} required placeholder="masalan: Model B" />
                     <div className="modal-box">
                         <p className="modal-subtext">Holat</p>
                         <div className="toggle">
 
-                            <input type="checkbox" id="holat" />
+                            <input type="checkbox" id="holat"  />
                             <label className="toggle-label" for="holat">Toggle</label>
 
                         </div>
                     </div>
 
-                    <button type="submit" className="modal-btn">
+                    <button type="submit" onClick={() => { createCategory(cName) }}  className="modal-btn">
                         Qo'shish
                     </button>
                 </form>
             </div>
         </Modal>
 
-        <Modal show={addModal} w={300} >
-            <div className="modal-blok">
-                
-                <button className="close-btn" onClick={()=>setAddModal()}>
-                    <img src={Close} className="close-icon" alt="" />
-                </button>
-            
-            <h2 className="modal-title">Qo'shish</h2>
-            <form className="modal-form">
-                <p className="modal-text">
-                    Kategoriya nomi
-                </p>
-                <input type="text" className="modal-input" required placeholder="masalan: Model B" />
-                <div className="modal-box">
-                    <p className="modal-subtext">Holat</p>
-                    <div className="toggle">
-
-                        <input type="checkbox" id="holat" />
-                        <label className="toggle-label" for="holat">Toggle</label>
-
-                    </div>
-                </div>
-
-                <button type="submit" className="modal-btn">
-                    Qo'shish
-                </button>
-            </form>
-</div>
-</Modal>
-
-</section>
+    </section>
 
 
 </div>
